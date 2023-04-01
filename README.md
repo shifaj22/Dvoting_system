5e9da8f7f7031d1c3270386fc8ce7365b70df391
Sheefa <jalalisheefa@gmail.com>
2023-04-01 17:48:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+Decentralized Voting System
+
+Make Sure that you've Truffle and Ganache installed
+
+1. Clone the repo: git clone https://github.com/shifaj22/Blockchain_Voting.git
+
+2. Navigate to the folder: cd Blockchain_Voting
+
+3. Install dependencies: npm install.
+
+4. I have used Ganache app, make sure to download it from https://trufflesuite.com/ganache/, keep it running in the background by choosing the Quickstart Ethereum option.
+
+5. Open the terminal and deploy the smart contract using: truffle migrate --reset. Run the BlockVoting application by using the command: npm run dev
+
+6. To Cast Vote,
+
+6.1 First Connect to localhost:7545 network in Metamask. You have to manually add the Ganache blockchain in networks, the chain id is 1337.
+
+6.2 Secondly, copy one of the private key from the Ganache app and import it in the Metamask account section.
+
+6.3 You should see 99-100 ETH in your address once you import it.
+
+6.4 Manually connect Metamask to the cast vote page by clicking on the Not Connected option which is shown left side of your address and connect it to the website.
+
+6.5 Once it shows connected, you can successfully cast your vote
---
+++ contracts/Election.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.16;
+
+contract Election {
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        string party;
+        uint voteCount;
+    }
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+    constructor () public {
+        addCandidate("Rachel Green","Bharatiya Janata Party");
+        addCandidate("Monica Geller","Indian National Congress");
+        addCandidate("Phoebe Buffay","Communist Party Of India (Marxist)");
+        addCandidate("Joey Tribbiani","All India Trinamool Congress");
+        addCandidate("Chandler Bing","Bahujan Samaj Party");
+        addCandidate("Ross Geller","Aam Admi Party");
+    }
+
+    function addCandidate (string memory name,string memory party) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, name,party, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
+    }
+}
